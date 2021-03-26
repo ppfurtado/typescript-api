7de0f3152dd3a0841e37e161e72250388eb2ae16
ppfurtado <furtado.pedropaulo@gmail.com>
2021-03-25 21:20:51 -0300

    configuration jest complete

---
+++ test/jest-setup.ts
@@ -0,0 +1,8 @@
+import {SetupServer} from '@src/server'
+import superTest from 'supertest'
+
+beforeAll(()=>{
+  const server = new SetupServer()
+  server.init()
+  global.testRequest = superTest(server.getApp())
+})
